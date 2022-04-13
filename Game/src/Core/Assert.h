0b8a62ce687a53bad120235fcb5b61d905e27e2e
AdrielMarchena <adriel_contato@hotmail.com>
2022-04-13 20:15:06 -0300

    Add macro for log a warn on a if and if not

--- Game/src/Core/Assert.h
+++ Game/src/Core/Assert.h
@@ -4,3 +4,5 @@
 #include "Log/Log.h"
 
 #define GAME_CORE_ASSERT(expression,...) if(!(expression)) { GAME_LOG_WARN(__VA_ARGS__); debug_break(); }
+#define GAME_WARN_IF(expression,...) if(expression) { GAME_LOG_WARN(__VA_ARGS__); }
+#define GAME_WARN_IF_NOT(expression,...) if(!(expression)) { GAME_LOG_WARN(__VA_ARGS__); }
