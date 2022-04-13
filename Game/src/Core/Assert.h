ced56343e8006ae231f8660534d89ab4afe23ea2
AdrielMarchena <adriel_contato@hotmail.com>
2022-04-13 20:20:08 -0300

    Add debug only macros

--- Game/src/Core/Assert.h
+++ Game/src/Core/Assert.h
@@ -6,3 +6,17 @@
 #define GAME_CORE_ASSERT(expression,...) if(!(expression)) { GAME_LOG_WARN(__VA_ARGS__); debug_break(); }
 #define GAME_WARN_IF(expression,...) if(expression) { GAME_LOG_WARN(__VA_ARGS__); }
 #define GAME_WARN_IF_NOT(expression,...) if(!(expression)) { GAME_LOG_WARN(__VA_ARGS__); }
+
+#ifdef GAME_DEBUG
+
+#define GAME_DEBUG_CORE_ASSERT(expression,...) if(!(expression)) { GAME_LOG_WARN(__VA_ARGS__); debug_break(); }
+#define GAME_DEBUG_WARN_IF(expression,...) if(expression) { GAME_LOG_WARN(__VA_ARGS__); }
+#define GAME_DEBUG_WARN_IF_NOT(expression,...) if(!(expression)) { GAME_LOG_WARN(__VA_ARGS__); }
+
+#else
+
+#define GAME_DEBUG_CORE_ASSERT(expression,...)
+#define GAME_DEBUG_WARN_IF(expression,...) 
+#define GAME_DEBUG_WARN_IF_NOT(expression,...)
+
+#endif
