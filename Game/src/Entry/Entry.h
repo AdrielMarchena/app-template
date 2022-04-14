a3502da2765947bf1898053d9a7ea1df44e01dc3
AdrielMarchena <adriel_contato@hotmail.com>
2022-04-14 15:09:18 -0300

    Minor change to benchmark dir

--- Game/src/Entry/Entry.h
+++ Game/src/Entry/Entry.h
@@ -4,6 +4,20 @@
 #include "Application.h"
 #include "Debug/Intrumentator.h"
 
+#ifdef GAME_DEBUG
+
+#define BENCHMARKING_CONFIGURATION "Debug/"
+
+#elif GAME_RELEASE
+
+#define BENCHMARKING_CONFIGURATION "Release/"
+
+#else
+
+#define BENCHMARKING_CONFIGURATION ""
+
+#endif
+
 extern Game::Application* Game::CreateApplication(int argc, char** argv);
 
 int main(int argc, char** argv)
@@ -11,16 +25,19 @@ int main(int argc, char** argv)
 	try
 	{
 		Game::Log::Init();
-		GAME_PROFILE_BEGIN_SESSION("initialization", "game_initialization_profile.json");
+
+		std::string benchmarkingDir = "benchmarking/" + std::string(BENCHMARKING_CONFIGURATION);
+
+		GAME_PROFILE_BEGIN_SESSION("initialization", benchmarkingDir + "game_initialization_profile.json");
 		Game::Application* game = Game::CreateApplication(argc, argv);
 		Game::Application::m_AppInstance = game;
 		GAME_PROFILE_END_SESSION();
 
-		GAME_PROFILE_BEGIN_SESSION("running", "game_running_profile.json");
+		GAME_PROFILE_BEGIN_SESSION("running", benchmarkingDir + "game_running_profile.json");
 		game->Run();
 		GAME_PROFILE_END_SESSION();
 
-		GAME_PROFILE_BEGIN_SESSION("shutdown", "game_shutdown_profile.json");
+		GAME_PROFILE_BEGIN_SESSION("shutdown", benchmarkingDir + "game_shutdown_profile.json");
 		delete game;
 		GAME_PROFILE_END_SESSION();
 	}
