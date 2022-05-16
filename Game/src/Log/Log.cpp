dc26056737caca2d48989b280a697a538c8c7f36
Adriel Marchena Santos <adriel_contato@hotmail.com>
2022-05-15 22:43:01 -0300

    Minor changes to logging

--- Game/src/Log/Log.cpp
+++ Game/src/Log/Log.cpp
@@ -13,7 +13,7 @@ namespace Game
 		try
 		{
 			spdlog::set_pattern("%^[%T] %n: %v%$");
-			s_CoreLogger = spdlog::stdout_color_mt("Base");
+			s_CoreLogger = spdlog::stdout_color_mt("Core");
 			s_CoreLogger->set_level(spdlog::level::trace);
 
 			s_ClientLogger = spdlog::stdout_color_mt("App");
